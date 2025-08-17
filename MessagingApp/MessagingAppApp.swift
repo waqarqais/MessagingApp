0811c21348eb2ef24d03422e630467323dfacdc2
waqarqais <waqarqais15@gmail.com>
2025-08-17 14:06:06 +0500

    Initial Commit

---
+++ MessagingApp/MessagingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessagingAppApp.swift
+//  MessagingApp
+//
+//  Created by Vakr Kais on 17/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct MessagingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
